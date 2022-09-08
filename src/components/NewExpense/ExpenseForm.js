7c359363dfd2bb37d1b097d86005227fa3edf962
dopamine-s <motor55005@gmail.com>
2022-09-08 10:37:27 +0300

    adds user input and multiple states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,11 +1,32 @@
+import { useState } from 'react';
+
 import './ExpenseForm.css';
 
 export default function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
   return <form>
     <div className="new-expense__controls">
       <div className="new-expense__control">
         <label>Title</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={titleChangeHandler}
+        />
       </div>
     </div>
     <div className="new-expense__controls">
@@ -15,6 +36,7 @@ export default function ExpenseForm() {
           type="number"
           min="0.01"
           step="0.01"
+          onChange={amountChangeHandler}
         />
       </div>
     </div>
@@ -25,6 +47,7 @@ export default function ExpenseForm() {
           type="date"
           min="2019-01-01"
           max="2022-12-31"
+          onChange={dateChangeHandler}
         />
       </div>
     </div>
