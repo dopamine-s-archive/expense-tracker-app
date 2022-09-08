8ca966473c54feb3757a37dce70a32995511b3ad
dopamine-s <motor55005@gmail.com>
2022-09-08 17:21:45 +0300

    adds submission and two-way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -19,12 +19,28 @@ export default function ExpenseForm() {
     setEnteredDate(event.target.value);
   };
 
-  return <form>
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+
+    console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
+  return <form onSubmit={submitHandler}>
     <div className="new-expense__controls">
       <div className="new-expense__control">
         <label>Title</label>
         <input
           type="text"
+          value={enteredTitle}
           onChange={titleChangeHandler}
         />
       </div>
@@ -36,6 +52,7 @@ export default function ExpenseForm() {
           type="number"
           min="0.01"
           step="0.01"
+          value={enteredAmount}
           onChange={amountChangeHandler}
         />
       </div>
@@ -47,6 +64,7 @@ export default function ExpenseForm() {
           type="date"
           min="2019-01-01"
           max="2022-12-31"
+          value={enteredDate}
           onChange={dateChangeHandler}
         />
       </div>
