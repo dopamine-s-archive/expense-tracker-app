28a4c87fe26529852c1d42d700411c3a0d63e9b2
dopamine-s <motor55005@gmail.com>
2022-09-09 13:31:32 +0300

    makes ExpenseItem a stateless component removing demo state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,15 @@
-import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 export default function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHander = () => {
-    setTitle('Updated');
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHander}>Change Title</button>
     </Card>
   );
 }
