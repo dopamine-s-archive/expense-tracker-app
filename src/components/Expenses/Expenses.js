9f527f44a259ef21092eb3a4b6f4cd2b132b4e51
dopamine-s <motor55005@gmail.com>
2022-09-12 22:58:19 +0300

    adds All years value. Adds conditional output

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,7 @@ import Card from '../UI/Card';
 import './Expenses.css';
 
 export default function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState('2022');
+  const [filteredYear, setFilteredYear] = useState('ALL');
 
   const yearFilterChangeHandler = selectedYearValue => {
     setFilteredYear(selectedYearValue);
@@ -14,6 +14,22 @@ export default function Expenses(props) {
   console.log('in Expenses.js - yearValue', filteredYear);
   console.log('in Expenses.js - props', props)
 
+  const filteredExpenses = filteredYear !== 'ALL' ?
+  props.expenses.filter((expense) => expense.date.getFullYear().toString() === filteredYear) : props.expenses;
+
+  let expensesContent = <p>No expenses found.</p>
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -21,16 +37,7 @@ export default function Expenses(props) {
           selected={filteredYear}
           onChangeYearFilter={yearFilterChangeHandler}
         />
-        {
-          props.expenses.filter((expense) => expense.date.getFullYear().toString() === filteredYear).map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))
-        }
+        {expensesContent}
       </Card>
     </div>
   );
--- src/components/ExpensesFilter/ExpensesFilter.js
+++ src/components/ExpensesFilter/ExpensesFilter.js
@@ -16,6 +16,7 @@ export default function ExpensesFilter(props) {
           value={props.selected}
           onChange={changeExpenseYearHandler}
         >
+          <option value='ALL'>ALL</option>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
